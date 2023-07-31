73735a93fc1cd70acd6617ff5c7fd74b90918dc9
sachin2503 <115262637+sachin2503@users.noreply.github.com>
2023-07-31 17:00:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// const chats = document.querySelector("[allChats]");
+const allchats = document.querySelector(".chats-container");
+
+
+    function visiblee(){
+        allchats.classList.add("active");
+    };
+     
